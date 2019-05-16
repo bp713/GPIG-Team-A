fc65be4e8c7ccfcb102f471f4f005272cb07bbd9
bp713 <pepersben@gmail.com>
2019-05-16 12:59:50 +0100

    Update and rename test.py to lookup_location.py

--- Server/lookup_location.py
+++ Server/lookup_location.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import time
 import swagger_client
 from swagger_client.rest import ApiException
+
 from pprint import pprint
 # create an instance of the API class
 api_instance = swagger_client.GeocodingApi()
