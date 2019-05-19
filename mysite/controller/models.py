e9cbb48c397d3d04abe78f7bd2aea7ec37b150af
James Callan <jc1850@york.ac.uk>
2019-05-19 14:54:27 +0100

    Added route server and began safety server

--- mysite/controller/models.py
+++ mysite/controller/models.py
@@ -10,3 +10,5 @@ class Controller(models.Model):
 class Courier(models.Model):
     controller = models.ForeignKey(Controller, on_delete=models.CASCADE)
     location = models.CharField(max_length = 50, default = '')
+    longitude = models.FloatField(default=0)
+    latitude = models.FloatField(default=0)
--- mysite/controller/views.py
+++ mysite/controller/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import  get_object_or_404, render
 from django.http import HttpResponse
 from .models import Controller
+import os
+from django.conf import settings
+import django
+import json
 
 def controller(request, controller_id):
     controller = get_object_or_404(Controller, pk=controller_id)
     context = { 'controller' : controller}
     return render(request, 'controller/controller.html', context)
 
-
+def route(request):
+    json_file = open(os.path.join(settings.BASE_DIR, 'routejson.txt'))
+    data = json.load(json_file)
+    return HttpResponse(json.dumps(data), content_type="application/json")
