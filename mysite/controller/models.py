b555b787cd0b9cf76f80794e2cae1b6e56593dc8
Tom Grant <tom.grant760@gmail.com>
2019-05-20 21:39:46 +0100

    Add Panic Fragment  - has different levels of issues Add Settings Fragment  - will be used to set server IP

--- mysite/controller/models.py
+++ mysite/controller/models.py
@@ -3,10 +3,17 @@ from django.db import models
 # Create your models here.
 class Controller(models.Model):
     name = models.CharField(max_length = 50)
-    
+
     def __str__(self):
         return self.name
 
 class Courier(models.Model):
     controller = models.ForeignKey(Controller, on_delete=models.CASCADE)
     location = models.CharField(max_length = 50, default = '')
+    
+    #required for auth:
+    email = models.CharField(max_length = 50, default = '')
+    cred_id = models.ByteField()
+    cred_pub_key = models.ByteField()
+    registration_challenge = models.ByteField()
+    authentication_challenge = models.ByteField()
