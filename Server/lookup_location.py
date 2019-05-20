493d0b58c072b3150df75fe13d422dd063829eac
bp713 <pepersben@gmail.com>
2019-05-20 16:03:15 +0100

    modified code to make it easier to use with interface

--- Server/lookup_location.py
+++ Server/lookup_location.py
@@ -12,12 +12,13 @@ limit = 56 # int | Specify the maximum number of returned results (optional)
 reverse = False # bool | Set to true to do a reverse Geocoding request, see point parameter (optional)
 point = 'point_example' # str | The location bias in the format 'latitude,longitude' e.g. point=45.93272,11.58803 (optional)
 provider = 'provider_example' # str | Can be either, default, nominatim, opencagedata (optional)
-
-try:
-    # Execute a Geocoding request
-    api_response = api_instance.geocode_get(key, q=q, locale=locale, limit=limit, reverse=reverse, point=point, provider=provider)
-    response = api_response.to_dict()
-    #pprint(api_response)
-    print(response['hits'][1])
-except ApiException as e:
-    print("Exception when calling GeocodingApi->geocode_get: %s\n" % e)
+def lookup_location(key,q,limit,locale='en'):
+	try:
+	    # Execute a Geocoding request
+	    api_response = api_instance.geocode_get(key, q=q, locale=locale, limit=limit)
+	    response = api_response.to_dict()
+	    #pprint(api_response)
+	    print(response['hits'][1])
+	except ApiException as e:
+	    print("Exception when calling GeocodingApi->geocode_get: %s\n" % e)
+lookup_location(key,q,limit)
