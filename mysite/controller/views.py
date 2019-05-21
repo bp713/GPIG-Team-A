0524f4e07e8804b4ec8895df4b74427bc12223b8
James Callan <jc1850@york.ac.uk>
2019-05-21 13:47:09 +0100

    began to put swagger client into server

--- mysite/controller/views.py
+++ mysite/controller/views.py
@@ -5,6 +5,7 @@ import os
 from django.conf import settings
 import django
 import json
+import controller.Routetest
 
 def controller(request, controller_id):
     controller = get_object_or_404(Controller, pk=controller_id)
@@ -15,3 +16,17 @@ def route(request):
     json_file = open(os.path.join(settings.BASE_DIR, 'routejson.txt'))
     data = json.load(json_file)
     return HttpResponse(json.dumps(data), content_type="application/json")
+
+def routeCalc(request):
+    lang1 = 1
+    long1 = 1
+    point1 = '%s,%s' %(lang1,long1)
+
+    lang2 = 1
+    long2 = 1
+    point2 = '%s,%s' %(lang1,long1)
+    points = [point1, point2]
+    
+    makeroute(Routetest.point, Routetest.key, Routetest.maxtraveltime)
+
+    
