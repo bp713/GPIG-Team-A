6ac1fc6accf7080e99acb9a102e0c25699331715
James Callan <jc1850@york.ac.uk>
2019-05-27 14:35:29 +0100

    added http interface to safety features

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,7 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('controller/', include('controller.urls')),
-    path('authentication/', include('authentication.urls')),
-    path('.well-known/', include('authentication.assetLinks')),
     path('admin/', admin.site.urls),
+    path('safety/', include('safety.urls'))
 ]
